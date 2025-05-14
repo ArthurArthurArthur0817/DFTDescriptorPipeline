4759c958ca8ede57a96a21474860fc6720456c87
peculab <pecu610@gmail.com>
2025-05-14 13:14:05 +0800

    import

--- descriptors/__init__.py
+++ descriptors/__init__.py
@@ -1,3 +1,18 @@
-__version__ = "0.1.0"
-from .extractor import extract_homo_lumo, extract_dipole_moment, extract_polarizability, extract_nbo_section
-from .sterimol import find_oh_bonds, find_c1_c2
+__version__ = "0.1.1"
+
+# From extractor.py
+from .extractor import (
+    extract_homo_lumo,
+    extract_dipole_moment,
+    extract_polarizability,
+    extract_nbo_section
+)
+
+# From sterimol.py
+from .sterimol import (
+    find_oh_bonds,
+    find_c1_c2
+)
+
+# From aggregate.py
+from .aggregate import generate_feature_table
